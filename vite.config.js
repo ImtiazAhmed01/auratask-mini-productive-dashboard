45bfc1ab2abe6ed7dab6c13afed37257c882d56d
ImtiazAhmed01 <imtiazahm21@gmail.com>
2025-05-29 17:59:49 +0600

    fixed errors

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}"
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#00C853',
-        secondary: '#C8F7DC',
-        textgray: '#666666',
-        darkgray: '#2C2C2C',
-        inputborder: '#DADADA',
-      }
-    }
-  },
-  plugins: [require('daisyui')],
-  daisyui: {
-    themes: ['light', 'dark'], // enable light and dark themes
-  },
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  // theme: {
+  //   extend: {
+  //     colors: {
+  //       primary: '#00C853',
+  //       secondary: '#C8F7DC',
+  //       textgray: '#666666',
+  //       darkgray: '#2C2C2C',
+  //       inputborder: '#DADADA',
+  //     }
+  //   }
+  // }
+
+  plugins: [react(), tailwindcss()],
+})
